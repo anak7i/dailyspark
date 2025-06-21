67878c516adec10a992f777b62af0ad1eb9e1b20
Aaron <imlizhi@gmail.com>
2025-06-21 13:53:57 +0800

    Update version code to 4

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 21
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.1"
     }
 
